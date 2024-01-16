229b88fbd8643f512ec52e0ad76cf6d9f3d78910
Nicolai Klokmose <angryniczorn@gmail.com>
2024-01-16 09:59:16 +0100

    1. User Story (Domain Model + Red Test)

--- src/main/java/com/booleanuk/StudentSelector.java
+++ src/main/java/com/booleanuk/StudentSelector.java
@@ -2,4 +2,13 @@ package com.booleanuk;
 
 public class StudentSelector {
 
+    public boolean run() {
+       showMenu();
+       return false;
+    }
+
+    public void showMenu() {
+
+    }
+
 }
--- src/test/java/com/booleanuk/core/CoreTest.java
+++ src/test/java/com/booleanuk/core/CoreTest.java
@@ -1,4 +1,15 @@
 package com.booleanuk.core;
 
+import com.booleanuk.StudentSelector;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CoreTest {
+
+    @Test
+    public void testRun() {
+        StudentSelector s = new StudentSelector();
+        Assertions.assertTrue(s.run());
+    }
+
 }
